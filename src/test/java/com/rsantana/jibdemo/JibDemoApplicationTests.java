0bfaeb3a1854c6adac584ec96c48bebea3aa5c7e
Rafael Moreti Santana <rafaelmoretisantana@gmail.com>
2019-05-15 21:09:17 +0100

    Jib configuration

---
+++ src/test/java/com/rsantana/jibdemo/JibDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rsantana.jibdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JibDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
